466d2dd2c6c8d6467c2863ab6ff973f711ca8876
unknown <anshuldhakate11@gmail.com>
2024-01-12 22:42:28 +0530

    new add

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express= require("express")
 const connection= require("./db")
 const {userRouter}= require("./routes/user.routes")
 const {noteRouter}= require("./routes/note.routes")
+const cors= require("cors")
 
 const app= express();
-
+app.use(cors())
 app.use(express.json())
 app.use("/users", userRouter)
 app.use("/notes", noteRouter)
